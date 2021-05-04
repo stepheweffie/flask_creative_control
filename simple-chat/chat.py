b9e040d7e06c4f53e1fb1b788cc8ec66f906f183
stepheweffie <stephie.maths@gmail.com>
2021-05-04 19:05:49 -0400

    chat.py Handler

--- simple-chat/chat.py
+++ simple-chat/chat.py
@@ -26,7 +26,9 @@ def index():
 if __name__ == "__main__":
     from gevent import pywsgi
     from geventwebsocket.handler import WebSocketHandler as Handler
-    server = pywsgi.WSGIServer(('https://cc-simple-chat.herokuapp.com', 5000), app, handler_class=Handler)
+
     Handler.upgrade_websocket()
+    server = pywsgi.WSGIServer(('https://cc-simple-chat.herokuapp.com', 5000), app, handler_class=Handler)
+
     print(Handler.connection_upgraded)
     server.serve_forever()
