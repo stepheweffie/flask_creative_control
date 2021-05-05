5d9100f07c069fee53056d3effe30ae1328c7acd
stepheweffie <stephie.maths@gmail.com>
2021-05-04 22:02:00 -0400

    redis

--- simple-chat/chat.py
+++ simple-chat/chat.py
@@ -1,21 +1,31 @@
 from gevent import monkey
 monkey.patch_all()
+import gevent
+import os
+import redis
+from redis import publish
 from flask import Flask, render_template
 from flask_sockets import Sockets
+REDIS_URL = os.environ.get('REDIS_URL')
+REDIS_CHAN = 'simple-chat'
 # from websocket import create_connection
 
 
 app = Flask(__name__)
 sockets = Sockets(app)
-
+r = redis.from_url(REDIS_URL)
 # ws = create_connection("wss://cc-simple-chat.herokuapp.com:8000")
 
 
 @sockets.route('/')
 def echo_socket(ws):
     while not ws.closed:
+        gevent.sleep(0.1)
         message = ws.receive()
         ws.send(message)
+        if message:
+            app.logger.info(f''.format(message))
+            r.publish(REDIS_CHAN, message)
 
 
 @app.route('/', methods=["GET", "POST"])
