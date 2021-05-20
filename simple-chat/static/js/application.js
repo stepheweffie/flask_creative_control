3a21355570643f17ab6794406823d97b97f69b18
stepheweffie <stephie.maths@gmail.com>
2021-05-20 12:57:56 -0400

    sockets.run

--- simple-chat/static/js/application.js
+++ simple-chat/static/js/application.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     if ("WebSocket in window") {
         const client = new ReconnectingWebSocket('wss://' + location.host + '/submit');
         const server = new ReconnectingWebSocket('wss://' + location.host + '/recieve');
-        client.onopen = function (event) {
+        client.onopen = function () {
             // Send a small message to the console once the connection is established */
             console.log('Connection open!');
         };
@@ -20,14 +20,16 @@ $(document).ready(function() {
 
         const handle = $("#handle")[0].value;
         const text = $("#chat-text")[0].value;
-        const message = (JSON.stringify({handle: handle, text: text, type: message}));
+        const message ={
+          type: 'message',
+          handle: handle,
+          text: text,
+          date: Date.now(),
+        };
 
         $("#input-form").on("submit", function (event) {
             event.preventDefault();
-            const handle = $("#handle")[0].value;
-            const text = $("#chat-text")[0].value;
-            console.log(handle, text);
-            server.send(JSON.stringify({handle: handle, text: text}));
+            server.send(JSON.stringify(message));
             $("#text-input")[0].value = "";
         });
     }
