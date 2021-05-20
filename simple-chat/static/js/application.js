4f6c05e3bdec7bf61bac8c72d1635f503bfdf071
stepheweffie <stephie.maths@gmail.com>
2021-05-20 18:28:50 -0400

    uris client/server

--- simple-chat/static/js/application.js
+++ simple-chat/static/js/application.js
@@ -1,9 +1,8 @@
 
 $(document).ready(function() {
     if ("WebSocket in window") {
-        const wsuri = 'wss://'+'cc-simple-chat.herokuapp.com:5000';
-        const client = new ReconnectingWebSocket(wsuri + '/submit');
-        const server = new ReconnectingWebSocket(wsuri + '/recieve');
+        const client = new ReconnectingWebSocket('wss://cc-simple-chat.herokuapp.com:8000/submit');
+        const server = new ReconnectingWebSocket('wss://cc-simple-chat.herokuapp.com:8000/recieve');
         client.onopen = function () {
             // Send a small message to the console once the connection is established */
             console.log('Connection open!');
